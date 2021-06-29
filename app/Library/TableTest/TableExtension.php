ca82098e6c6a5573581131f948398862cc788f53
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-30 02:50:46 +0900

    add TableParser

--- app/Library/TableTest/TableExtension.php
+++ app/Library/TableTest/TableExtension.php
@@ -14,9 +14,11 @@ final class TableExtension implements ExtensionInterface
     public function register(ConfigurableEnvironmentInterface $environment)
     {
         $environment
-        ->addBlockParser(new TableParser())
+            ->addBlockParser(new TableParser())
 
-        ->addBlockRenderer(Table::class, new TableRenderer())
-        ->addBlockRenderer()
+            ->addBlockRenderer(Table::class, new TableRenderer())
+            ->addBlockRenderer(TableSection::class, new TableSectionRenderer())
+            ->addBlockRenderer(TableRow::class, new TableRowRenderer())
+            ->addBlockRenderer(TableCell::class, new TableCellRenderer());
     }
 }
